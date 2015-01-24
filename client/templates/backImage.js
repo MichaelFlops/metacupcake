cd260a760ef79d487c96bb376b1b3d2347b3d086
michaelbetts <mwbetts@ucsc.edu>
2015-01-24 11:46:16 -0800

    fixed navbar + basic positioning

--- client/templates/backImage.js
+++ client/templates/backImage.js
@@ -1,7 +1,7 @@
-var images = ['back1.jpg', 'back2.jpg'];
+//var images = ['back1.jpg', 'back2.jpg'];
     
     //Build the img, then do a bit of maths to randomize load and append to a div. 
     //Add a touch off css to fade them badboys in all sexy like.
-$('<img class=" class="fade-in" src="images/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');    
+//$('<img class=" class="fade-in" src="images/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');    
 
 
