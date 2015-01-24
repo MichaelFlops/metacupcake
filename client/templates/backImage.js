a448baf05df6d05cf69108669809765cf29ebe05
michaelbetts <mwbetts@ucsc.edu>
2015-01-24 15:42:43 -0800

    MADE RANDOM IMAGE GENERATOR BIATCH, and added a nice fade in to the ui

--- client/templates/backImage.js
+++
@@ -1,7 +0,0 @@
-//var images = ['back1.jpg', 'back2.jpg'];
-    
-    //Build the img, then do a bit of maths to randomize load and append to a div. 
-    //Add a touch off css to fade them badboys in all sexy like.
-//$('<img class=" class="fade-in" src="images/' + images[Math.floor(Math.random() * images.length)] + '">').appendTo('#banner-load');    
-
-
---
+++ client/templates/backgroundImage.js
@@ -0,0 +1,11 @@
+// background image generator
+
+Template.backgroundImage.helpers ({
+	background: function() {
+	var myurls = ["images/back1.jpg", "images/back2.jpg", "images/back3.jpg"];
+	var the_random_url = Random.choice(myurls);
+	console.log(the_random_url);
+	return the_random_url;
+	// var background = the_random_url;
+	}
+});
