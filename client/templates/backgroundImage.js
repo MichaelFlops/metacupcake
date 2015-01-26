9ce16aa065b3ecd91dbbfcee9ddf7f33439c495b
michaelbetts <mwbetts@ucsc.edu>
2015-01-26 09:28:38 -0800

    added 2 photos to background array

--- client/templates/backgroundImage.js
+++ client/templates/backgroundImage.js
@@ -2,7 +2,7 @@
 
 Template.backgroundImage.helpers ({
 	background: function() {
-	var myurls = ["images/back1.jpg", "images/back2.jpg", "images/back3.jpg", "images/back4.jpg"];
+	var myurls = ["images/back1.jpg", "images/back2.jpg", "images/back3.jpg", "images/back4.jpg", "images/back5.jpg", "images/back6.jpg"];
 	var the_random_url = Random.choice(myurls);
 	console.log(the_random_url);
 	return the_random_url;
